6bab365df8f3841949a58477b45993263744c514
dwleonhardt <dwleonhardt@gmail.com>
2017-05-05 16:55:19 -0600

    working price search

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,26 @@
 $( document ).ready(function() {
   var id = config.id;
   var secret = config.secret;
-  function Input(city, state){
+  function Input(city, state, price){
     this.city = city;
     this.state = state;
-    this.radius = 800;
+    this.price = price;
   }
-  console.log(id);
-  // Search.prototype.radius = function(miles){
-  //   this.radius =
-  // };
+
 
   $('form').submit(function search(event){
     event.preventDefault();
     var $city = $('#city').val();
     var $state = $('#state').val();
-    var input = new Input($city, $state);
-    var url = 'https://api.foursquare.com/v2/venues/explore?client_id='+config.id+'&client_secret='+config.secret+'&v=20170401&mode=url&near='+input.city+','+input.state+'&q=food';
+    var $price = $('#price').val();
+    var input = new Input($city, $state, $price);
+    var url = 'https://api.foursquare.com/v2/venues/explore?client_id='+id+'&client_secret='+secret+'&v=20170401&mode=url&near='+input.city+','+input.state+'&price='+input.price+'&q=food';
     var $call = $.getJSON(url);
     $call.done(function(data){
-        console.log(data);
+      var $restaurants = $(data.response.groups[0].items);
+      var pick = Math.floor(Math.random()*$restaurants.length);
+      console.log($restaurants[pick].venue);
+
       });
   });
 

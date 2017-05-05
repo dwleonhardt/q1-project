139c316b82f1cd4883ed5e4c12a61e6737140738
dwleonhardt <dwleonhardt@gmail.com>
2017-05-05 17:41:00 -0600

    changed api call from food to restaurant

--- assets/script.js
+++ assets/script.js
@@ -6,26 +6,23 @@ $( document ).ready(function() {
     this.state = state;
     this.price = price;
   }
-
-
   $('form').submit(function search(event){
     event.preventDefault();
     var $city = $('#city').val();
     var $state = $('#state').val();
     var $price = $('#price').val();
+    var $result = $('<p></p>');
     var input = new Input($city, $state, $price);
-    var url = 'https://api.foursquare.com/v2/venues/explore?client_id='+id+'&client_secret='+secret+'&v=20170401&mode=url&near='+input.city+','+input.state+'&price='+input.price+'&q=food';
+    var url = 'https://api.foursquare.com/v2/venues/explore?client_id='+id+'&client_secret='+secret+'&v=20170401&mode=url&near='+input.city+','+input.state+'&price='+input.price+'&q=restaurant';
     var $call = $.getJSON(url);
     $call.done(function(data){
       var $restaurants = $(data.response.groups[0].items);
       var pick = Math.floor(Math.random()*$restaurants.length);
-      console.log($restaurants[pick].venue);
-
+      $('form').append($result);
+      $result.html($restaurants[pick].venue.name);
+      // console.log($restaurants[pick].venue);
       });
   });
-
-
-
 });
 
 

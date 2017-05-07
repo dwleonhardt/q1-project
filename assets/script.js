d8dd6a02ffa4c4764fc5a7b9f309ad4e60a879e0
dwleonhardt <dwleonhardt@gmail.com>
2017-05-07 15:39:56 -0600

    form to geocode to random selection working

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,47 @@
+
 var map;
 var infoWindow;
 var service;
 
-function initMap() {
+
+function Input(city, state, price){
+  this.city = city;
+  this.state = state;
+  this.price = price;
+  }
+  $('form').submit(function search(event){
+    event.preventDefault();
+    var $city = $('#city').val();
+    var $state = $('#state').val();
+    var $price = $('#price').val();
+    var $result = $('<p></p>');
+    var input = new Input($city, $state, $price);
+    var geo = 'https://maps.googleapis.com/maps/api/geocode/json?address='+$city+'+'+$state+'+'+'&region=us&key=AIzaSyBauiiK4RzU0EjgyAggzpVg3ogJs-CnWTg';
+    var $call = $.getJSON(geo);
+    $call.done(function(data){
+      var location = data.results[0].geometry.location;
+      initMap(location);
+      });
+  });
+
+
+
+function initMap(location) {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {
-      lat: -33.867,
-      lng: 151.206
+      lat: location.lat,
+      lng: location.lng
     },
     zoom: 15,
+
     styles: [{
       stylers: [{ visibility: 'simplified' }]
     },
     {
       elementType: 'labels',
       stylers: [{ visibility: 'on' }]
-    }]
+    },
+  ],
   });
 
   infoWindow = new google.maps.InfoWindow();
@@ -35,6 +61,7 @@ function performSearch() {
 }
 
 function callback(results, status) {
+  var picker = [];
   if (status !== google.maps.places.PlacesServiceStatus.OK) {
     // console.error(status);
     return;
@@ -43,10 +70,12 @@ function callback(results, status) {
     // var request = {
     //   placeId: result
     // };
+    picker.push(result);
+    // addMarker(result);
 
-    addMarker(result);
-    console.log();
   }
+  console.log(picker[Math.floor(Math.random() * picker.length)]);
+  addMarker(picker[Math.floor(Math.random() * picker.length)]);
 }
 
 function addMarker(place) {
